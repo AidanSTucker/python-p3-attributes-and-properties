2a05b028bffac35fb5972da25ad7df62bc587904
elpatron <aidanseantucker@gmail.com>
2023-12-18 15:24:46 -0500

    nvm, I', not as smart as I previously thought

--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,33 @@
-#!/usr/bin/env python3
-
-APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
-]
-
 class Person:
-    pass
+    approved_jobs = [
+        "Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", 
+        "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"
+    ]
+
+    def __init__(self, name="", job=""):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            self._name = ""
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in self.approved_jobs and value != "":
+            print("Job must be in list of approved jobs.")
+            self._job = ""
+        else:
+            self._job = value
